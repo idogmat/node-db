44d5b83bb46465ee0c985903ffa0874f2654de61
unknown <40455730+idogmat@users.noreply.github.com>
2021-09-20 23:54:42 +0300

    correct commit

---
+++ keys/keys.dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  MONGODB_URI: 'mongodb+srv://Jack:Id1885598@cluster0.f5xcu.mongodb.net/test',
+  SESSION_SECRET: 'some secret value',
+  SENDGRID_API_KEY: 's%3AzZ-fmpZywHlzEzFF72OQJ1oVvkiXjim0.8Wj0Qxs8EwebLNL1vfjXCiTJ5P5XLBCB8ld4ar5ffiE',
+  EMAIL_FROM: 'nodejs@email.ru',
+  BASE_URL: 'http://localhost:3000'
+}
