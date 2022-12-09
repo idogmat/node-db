ef36302f06416f1acefa3d89775a6c92b2f4c485
Jack <40455730+idogmat@users.noreply.github.com>
2022-12-09 19:03:11 +0300

    rm-nm

--- emails/registration.js
+++ emails/registration.js
@@ -9,7 +9,7 @@ module.exports = function(email) {
       <h1>Добро пожаловать в наш магазин</h1>
       <p>Вы успешно создали аккаунт c email - ${email}</p>
       <hr />
-      <a href="${keys.BASE_URL}">Магазин курсов</a>
+      <a href="${keys.BASE_URL}">Магазин</a>
     `
   }
 }
